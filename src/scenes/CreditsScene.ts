2d701b5025aac5a0ba157fc66d147059526df98e
Alexander Thurn <athurn84@gmail.com>
2026-06-25 15:04:25 +0200

    privacy

--- src/scenes/CreditsScene.ts
+++ src/scenes/CreditsScene.ts
@@ -4,7 +4,12 @@ import { createFocusableButton } from '../input/FocusableButton';
 import type { MenuActionsHost } from '../input/MenuActionsHost';
 import type { UiAction } from '../input/UiMenuController';
 import { createMenuBackground } from '../ui/MenuBackground';
-import { kewlBlockGap, kewlLineHeight, kewlText } from '../ui/KewlFont';
+import { kewlBlockGap, kewlLineHeight, kewlMeasuredSize, kewlText, kewlTextLeftInset } from '../ui/KewlFont';
+
+const FOOTER_MARGIN = 21;
+const FOOTER_FONT_SIZE = 21;
+const IMPRINT_URL = 'https://feuerware.com/2025/imprint.html';
+const PRIVACY_URL = 'https://feuerware.com/2025/privacy.html';
 
 export class CreditsScene extends Container implements MenuActionsHost {
   private menuActions: UiAction[] = [];
@@ -45,6 +50,33 @@ export class CreditsScene extends Container implements MenuActionsHost {
     body.position.set(centerX, 100 + kewlLineHeight(48) + kewlBlockGap(48));
     this.addChild(body);
 
+    const footerLineH = kewlLineHeight(FOOTER_FONT_SIZE);
+    const footerY = DESIGN.height - FOOTER_MARGIN - footerLineH;
+
+    this.addChild(
+      this.makeFooterLink(
+        'credits-imprint',
+        'Imprint',
+        FOOTER_MARGIN - kewlTextLeftInset(FOOTER_FONT_SIZE),
+        footerY,
+        IMPRINT_URL,
+      ),
+    );
+
+    const privacyText = 'Data Privacy';
+    const privacyW = kewlMeasuredSize(privacyText, FOOTER_FONT_SIZE).width;
+    this.addChild(
+      this.makeFooterLink(
+        'credits-privacy',
+        privacyText,
+        DESIGN.width - FOOTER_MARGIN - privacyW - 20,
+        footerY,
+        PRIVACY_URL,
+        'right',
+        privacyW,
+      ),
+    );
+
     this.addChild(
       this.makeButton('credits-back', 'Back', centerX - 100, DESIGN.height - 100, 200, this.onBack),
     );
@@ -67,6 +99,29 @@ export class CreditsScene extends Container implements MenuActionsHost {
       center: true,
       fontSize: 20,
       onPress: onClick,
+    });
+    this.menuActions.push(action);
+    return view;
+  }
+
+  private makeFooterLink(
+    id: string,
+    label: string,
+    x: number,
+    y: number,
+    url: string,
+    align: 'left' | 'right' = 'left',
+    w?: number,
+  ): Container {
+    const { view, action } = createFocusableButton({
+      id,
+      label,
+      x,
+      y,
+      w,
+      align,
+      fontSize: FOOTER_FONT_SIZE,
+      onPress: () => window.open(url, '_blank', 'noopener,noreferrer'),
     });
     this.menuActions.push(action);
     return view;
